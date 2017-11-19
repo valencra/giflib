640a78c1d0e66303e97fed177cc36295efe19e45
valencra <valencra@mcmaster.ca>
2017-11-19 11:15:32 -0500

    Added a gif repository class autowired into gif controller

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,6 +1,8 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
 import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,10 @@ import java.time.LocalDate;
 
 @Controller
 public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifs() {
         return "home";
@@ -17,7 +23,7 @@ public class GifController {
 
     @RequestMapping("/gif")
     public String gifDetails(ModelMap modelMap) {
-        Gif gif = new Gif("compiler-bot", LocalDate.of(2015, 2, 13),"Chris Ramacciotti", true);
+        Gif gif = gifRepository.findByName("android-explosion");
         modelMap.put("gif", gif);
         return "gif-details";
     }
